b7a4c0be94f081e081a7004ea3049ffa5c755ffa
j2m <jossyemunyi@gmail.com>
2021-09-03 16:38:51 +0300

    fetch random coctail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RandomDrinksComponent } from './components/random-drinks/random-drinks.component';
+import { CocktailDetailsComponent } from './components/cocktail-details/cocktail-details.component';
+import { CocktailServiceService } from './services/cocktail-service.service';
+
+
+const appRoutes: Routes = [
+  { path: 'coctails', component: RandomDrinksComponent },
+  { path: 'cocktail-details/:idDrink', component: CocktailDetailsComponent },
+  { path: '', redirectTo: '/coctails', pathMatch:'full'},
+  
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    RandomDrinksComponent
+    RandomDrinksComponent,
+    CocktailDetailsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/cocktail-service.service.ts
+++ src/app/services/cocktail-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
-import { Cocktail, drinks } from '../models/coctail';
+import { Cocktail } from '../models/coctail';
 import { forkJoin, Observable } from 'rxjs';
 
 const urlOrigin = new Array(5).fill(0);
@@ -10,7 +10,7 @@ const urlOrigin = new Array(5).fill(0);
   providedIn: 'root'
 })
 export class CocktailServiceService {
-
+  private _coctails: Cocktail [] = [];
   private baseUrl = `${environment.baseURL}/random.php`;
 
   constructor(private http: HttpClient,) { }
@@ -19,5 +19,24 @@ export class CocktailServiceService {
     const expectedURLS = urlOrigin.fill(this.http.get<Cocktail[]>(this.baseUrl), 0, 5)
     
     return forkJoin(expectedURLS)
+  }
+
+  getRandomCocktail():Observable<any> {
+    return this.http.get<Cocktail>(this.baseUrl)
+  }
+
+  public set setCoctails(value: Cocktail[]) {
+     this._coctails = value;
+  }
+
+  get coctails() {
+    return [...this._coctails];
+  }
+
+  getCoctailById(id: string | any) :Cocktail {
+    return {
+      ...this._coctails.find((coctail) =>coctail.idDrink == id
+      )
+    } as Cocktail
   }
 }
