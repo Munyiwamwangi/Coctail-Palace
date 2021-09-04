c734a9da5a543f3443c9c1e14a5bd62b232100a0
j2m <jossyemunyi@gmail.com>
2021-09-04 16:22:10 +0300

    deploying

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const port = process.env.PORT || 5000;
 function requireHTTPS(req, res, next) {
     // The 'x-forwarded-proto' check is for Heroku
     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
@@ -15,4 +16,8 @@ app.get("/*", function(req, res) {
   res.sendFile("index.html", {root: "dist/cocktailpalace/"}
 );
 });
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+        /* eslint-disable no-console */
+        console.log(`Listening: http://localhost:${port}`);
+        /* eslint-enable no-console */
+    });
